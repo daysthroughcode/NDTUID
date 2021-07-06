e3d7937ff5f05eb140933a8825e188e67a0d3472
Warren Villarosa <58594754+daysthroughcode@users.noreply.github.com>
2021-07-06 14:40:07 +0800

    Created Script and UML

---
+++ NDTUID.py
@@ -0,0 +1,20 @@
+class NDTUID:
+
+    # methode name in constructor
+    def __init__(self, n):
+        self.method = n
+
+    # Nested Dictionary to define method paremeters
+    NDTmethods = {
+        # Methods are defined as per ISO 9712
+        'AT': {'UID': 000000, },
+        'ET': {'UID': 000000, },
+        'IT': {'UID': 000000, },
+        'LT': {'UID': 000000, },
+        'MT': {'UID': 000000, },
+        'PT': {'UID': 000000, },
+        'RT': {'UID': 000000, },
+        'ST': {'UID': 000000, },
+        'UT': {'UID': 000000, },
+        'VT': {'UID': 000000, }
+    }
